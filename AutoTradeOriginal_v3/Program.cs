8a538e777055ea25b9aee054645987fe5e13c6fb
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 11:47:33 +0900

    プロジェクト ファイルを追加します。

---
+++ AutoTradeOriginal_v3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HighLowAuto
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //ミューテックス作成
+            Mutex app_mutex = new Mutex(false, "AutoTradeOriginal_v3");
+
+            //ミューテックスの所有権を要求する
+            if (app_mutex.WaitOne(0, false) == false)
+            {
+                MessageBox.Show("このアプリケーションは複数起動できません。");
+                return;
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
