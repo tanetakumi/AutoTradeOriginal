e15c7f0e5f0cb7f2f4e3bd1905074aa04fd25aca
tanetakumi <nutanetakumi@yahoo.co.jp>
2022-01-22 10:30:54 +0900

    first javascript data

---
+++ tabreset.js
@@ -0,0 +1,6 @@
+
+
+var num = document.getElementById('content_1').children.length;
+for(let i=num-1;i>0;i--){
+    document.getElementsByClassName('RecentlyOpenOptions_tabClose__2EbqE')[i].click();
+}
