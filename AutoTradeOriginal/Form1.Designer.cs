4e1e40b5840bc60e3749d1dcc1d11d5b452632dd
tanetakumi <nutanetakumi@yahoo.co.jp>
2022-02-02 07:59:13 +0900

    Create ProTrader branch

--- AutoTradeOriginal/Form1.Designer.cs
+++ AutoTradeOriginal/Form1.Designer.cs
@@ -34,32 +34,26 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tab_setting = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
             this.checkBox_real = new System.Windows.Forms.CheckBox();
-            this.textBox_username = new System.Windows.Forms.TextBox();
-            this.textBox_password = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.listBox_log = new System.Windows.Forms.ListBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.textBox_read = new System.Windows.Forms.TextBox();
-            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
             this.tabControl1.SuspendLayout();
             this.tab_setting.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            this.groupBox5.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
             this.splitContainer3.Panel1.SuspendLayout();
             this.splitContainer3.Panel2.SuspendLayout();
             this.splitContainer3.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button_start
             // 
-            this.button_start.Location = new System.Drawing.Point(3, 98);
+            this.button_start.Location = new System.Drawing.Point(15, 24);
             this.button_start.Margin = new System.Windows.Forms.Padding(2);
             this.button_start.Name = "button_start";
             this.button_start.Size = new System.Drawing.Size(96, 32);
@@ -71,7 +65,7 @@
             // button_stop
             // 
             this.button_stop.Enabled = false;
-            this.button_stop.Location = new System.Drawing.Point(103, 98);
+            this.button_stop.Location = new System.Drawing.Point(119, 24);
             this.button_stop.Margin = new System.Windows.Forms.Padding(2);
             this.button_stop.Name = "button_stop";
             this.button_stop.Size = new System.Drawing.Size(96, 32);
@@ -116,68 +110,36 @@
             this.splitContainer1.SplitterDistance = 887;
             this.splitContainer1.TabIndex = 19;
             // 
-            // groupBox5
+            // splitContainer3
             // 
-            this.groupBox5.Controls.Add(this.checkBox_real);
-            this.groupBox5.Controls.Add(this.textBox_username);
-            this.groupBox5.Controls.Add(this.textBox_password);
-            this.groupBox5.Controls.Add(this.label2);
-            this.groupBox5.Controls.Add(this.label1);
-            this.groupBox5.Location = new System.Drawing.Point(3, 3);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(203, 90);
-            this.groupBox5.TabIndex = 17;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "ユーザー設定";
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.checkBox_real);
+            this.splitContainer3.Panel1.Controls.Add(this.button_stop);
+            this.splitContainer3.Panel1.Controls.Add(this.button_start);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.listBox_log);
+            this.splitContainer3.Size = new System.Drawing.Size(263, 640);
+            this.splitContainer3.SplitterDistance = 65;
+            this.splitContainer3.TabIndex = 2;
             // 
             // checkBox_real
             // 
             this.checkBox_real.AutoSize = true;
-            this.checkBox_real.Location = new System.Drawing.Point(11, 19);
+            this.checkBox_real.Location = new System.Drawing.Point(15, 3);
             this.checkBox_real.Name = "checkBox_real";
             this.checkBox_real.Size = new System.Drawing.Size(108, 16);
             this.checkBox_real.TabIndex = 15;
             this.checkBox_real.Text = "リアル口座の使用";
             this.checkBox_real.UseVisualStyleBackColor = true;
-            // 
-            // textBox_username
-            // 
-            this.textBox_username.Location = new System.Drawing.Point(80, 40);
-            this.textBox_username.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox_username.Name = "textBox_username";
-            this.textBox_username.Size = new System.Drawing.Size(113, 19);
-            this.textBox_username.TabIndex = 4;
-            // 
-            // textBox_password
-            // 
-            this.textBox_password.Location = new System.Drawing.Point(80, 63);
-            this.textBox_password.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox_password.Name = "textBox_password";
-            this.textBox_password.Size = new System.Drawing.Size(113, 19);
-            this.textBox_password.TabIndex = 4;
-            this.textBox_password.UseSystemPasswordChar = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(5, 63);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 15);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "パスワード";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(6, 41);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "ユーザー名";
             // 
             // listBox_log
             // 
@@ -187,7 +149,7 @@
             this.listBox_log.ItemHeight = 12;
             this.listBox_log.Location = new System.Drawing.Point(0, 0);
             this.listBox_log.Name = "listBox_log";
-            this.listBox_log.Size = new System.Drawing.Size(263, 500);
+            this.listBox_log.Size = new System.Drawing.Size(263, 571);
             this.listBox_log.TabIndex = 12;
             // 
             // tabPage2
@@ -210,27 +172,6 @@
             this.textBox_read.TabIndex = 0;
             this.textBox_read.Text = "あいうえお\r\n";
             // 
-            // splitContainer3
-            // 
-            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer3.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer3.Name = "splitContainer3";
-            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer3.Panel1
-            // 
-            this.splitContainer3.Panel1.Controls.Add(this.groupBox5);
-            this.splitContainer3.Panel1.Controls.Add(this.button_stop);
-            this.splitContainer3.Panel1.Controls.Add(this.button_start);
-            // 
-            // splitContainer3.Panel2
-            // 
-            this.splitContainer3.Panel2.Controls.Add(this.listBox_log);
-            this.splitContainer3.Size = new System.Drawing.Size(263, 640);
-            this.splitContainer3.SplitterDistance = 136;
-            this.splitContainer3.TabIndex = 2;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -248,14 +189,13 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            this.groupBox5.ResumeLayout(false);
-            this.groupBox5.PerformLayout();
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
             this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
             this.splitContainer3.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
             this.splitContainer3.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -266,12 +206,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tab_setting;
         private System.Windows.Forms.ListBox listBox_log;
-        private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.CheckBox checkBox_real;
-        private System.Windows.Forms.TextBox textBox_username;
-        private System.Windows.Forms.TextBox textBox_password;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TextBox textBox_read;
         private System.Windows.Forms.SplitContainer splitContainer1;
